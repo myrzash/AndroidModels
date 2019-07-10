6da25163a265315bfd4c6bdc1101891f9f79aa75
myrzash <myrza.kozhahmet@gmail.com>
2019-07-10 11:46:05 +0600

    mvc started

--- app/src/main/java/kz/nice/modelsrxjava/mvc/MVCActivity.kt
+++ app/src/main/java/kz/nice/modelsrxjava/mvc/MVCActivity.kt
@@ -2,13 +2,41 @@ package kz.nice.modelsrxjava.mvc
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_mvc.*
 import kz.nice.modelsrxjava.R
 
 class MVCActivity : AppCompatActivity() {
+
+    private lateinit var adapter: ArrayAdapter<String>
+    val listValues: ArrayList<String> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mvc)
         setTitle("MVC")
+
+        adapter = ArrayAdapter(this, R.layout.row_layout, R.id.textViewName, listValues)
+        listViewMVC.adapter = adapter
+        listViewMVC.setOnItemClickListener(AdapterView.OnItemClickListener { parent, view, position, id ->
+            Toast.makeText(this, "clicked ${listValues.get(position)}", Toast.LENGTH_SHORT).show()
+        })
+        val list = arrayListOf<String>()
+        for (i in 0..10) {
+            list.add("USA")
+            list.add("UK")
+            list.add("China")
+            list.add("Italy")
+            list.add("Japan")
+        }
+        setListValues(list)
+    }
+
+    fun setListValues(values: List<String>) {
+        listValues.clear()
+        listValues.addAll(values)
+        adapter.notifyDataSetChanged()
     }
 }
